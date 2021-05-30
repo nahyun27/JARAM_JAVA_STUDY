b9628c08aa1fa40d54a11a77d5eb2883c698b39d
Nahyun Kim <62279962+nahyun27@users.noreply.github.com>
2021-05-30 16:25:58 +0900

    Update Animal.java

--- Chap 10/Animal.java
+++ Chap 10/Animal.java
@@ -4,13 +4,13 @@ public class Animal {
 	String name;
 	String kind;
 	int legCount;
-	public static void main(String[] args) {
-		
-	}
+	
 	public void move() {
-		
+		System.out.println(this.name+"움직움직");
+
 	}
 	public void eat() {
-		
+		System.out.println(this.name+ "냠냠");
+
 	}
 }

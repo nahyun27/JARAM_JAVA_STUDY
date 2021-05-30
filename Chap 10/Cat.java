1b5c8ef00fbb6ad66291fc01bbed467f4e9bfda9
Nahyun Kim <62279962+nahyun27@users.noreply.github.com>
2021-05-30 16:24:51 +0900

    Update Cat.java

--- Chap 10/Cat.java
+++ Chap 10/Cat.java
@@ -1,6 +1,14 @@
 package c.inheritance;
 
 public class Cat extends Animal {
-	//You must implement here
+    public void cry() {
+        System.out.println(this.name+"냐옹~");
+    }
+    
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Cat.setName("happy");
+        Cat.cry();
+    }
 
 }
